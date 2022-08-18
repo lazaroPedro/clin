abb72e2af715e6510fd34f0fa2882bf19493293e
lazaroPedro <lazaro.p806@gmail.com>
2022-08-17 22:54:58 -0300

    3 files modified, 1 added

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const activeClass = 'active';
 const list = document.querySelectorAll('li.navItem');
 
 addEventListener(click, document.i)*/
+
+const list = document.querySelectorAll('.navItem')
+function activeLink(){
+  list.forEach((item) => 
+  item.classList.remove('active'))
+  this.classList.add('active')
+}
+list.forEach((item) =>
+item.addEventListener('click', activeLink))
